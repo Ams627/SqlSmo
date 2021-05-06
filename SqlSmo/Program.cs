fdb42c7e17653c156773d3cf3ace9b09073ee8b6
Adrian Sims <adriansims62781@gmail.com>
2021-05-06 12:48:49 +0200

    Add project files.

---
+++ SqlSmo/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.Management.Smo;
+using Microsoft.SqlServer.Management.Smo.SqlEnum;
+using System.Collections.Specialized;
+
+namespace SqlSmo
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                Server srv = new Server();
+
+                srv.ConnectionContext.ServerInstance = "ServerName";
+                srv.ConnectionContext.LoginSecure = true;
+                string dbName = "I";
+
+                Database db = new Database();
+                db = srv.Databases[dbName];
+
+                StringBuilder sb = new StringBuilder();
+
+                foreach (Table tbl in db.Tables)
+                {
+                    ScriptingOptions options = new ScriptingOptions();
+                    options.ClusteredIndexes = true;
+                    options.Default = true;
+                    options.DriAll = true;
+                    options.Indexes = true;
+                    options.IncludeHeaders = true;
+
+                    StringCollection coll = tbl.Script(options);
+                    foreach (string str in coll)
+                    {
+                        sb.Append(str);
+                        sb.Append(Environment.NewLine);
+                    }
+                }
+                System.IO.StreamWriter fs = System.IO.File.CreateText("c:\\temp\\output.txt");
+                fs.Write(sb.ToString());
+                fs.Close();
+            }
+            catch (Exception ex)
+            {
+                var fullname = System.Reflection.Assembly.GetEntryAssembly().Location;
+                var progname = Path.GetFileNameWithoutExtension(fullname);
+                Console.Error.WriteLine($"{progname} Error: {ex.Message}");
+            }
+
+        }
+    }
+}
